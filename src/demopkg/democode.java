a6fe9bc4acd3fc20531aafdf55abe3a5f97d7420
Ujjawal231 <rahulujjawalkumar@gmail.com>
2022-05-03 17:53:05 +0530

    changed Print statement.

--- src/demopkg/democode.java
+++ src/demopkg/democode.java
@@ -2,6 +2,6 @@ package demopkg;
 
 public class democode {
     public static void main(String[] args) {
-        System.out.println("Rahul is a good boy");
+        System.out.println("Rahul is a good boys");
     }
 }
